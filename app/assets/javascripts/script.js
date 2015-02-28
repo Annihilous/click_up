5f77e2656d572932a1b84a35d25a6f3451c997a2
Annihilous <benjaminjnorton@gmail.com>
2015-02-28 17:38:09 -0500

    reset button added

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,14 +2,20 @@
 
 $(document).ready(function(){
 	$('.btn').mouseenter(function(){
-		$('.btn').fadeTo('fast', 0.5);
+		$(this).fadeTo('fast', 0.5);
 	});
 	$('.btn').mouseleave(function(){
-		$('.btn').fadeTo('fast', 1);
+		$(this).fadeTo('fast', 1);
 	});
 	var clicks = 0;
 	$('#clickMe').click(function(){
 		clicks ++;
+		$('h1').html(clicks);
+		// if clicks === 10;
+		// 	$(this).css("background", "blue");
+	})
+	$('#reset').click(function(){
+		clicks = 0;
 		$('h1').html(clicks);
 	})
 })
