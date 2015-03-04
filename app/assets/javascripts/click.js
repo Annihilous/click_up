626a61befac3d02bbf12aa703818d1538472b647
Annihilous <benjaminjnorton@gmail.com>
2015-03-04 09:41:11 -0500

    still fixing JS bug

--- app/assets/javascripts/click.js
+++ app/assets/javascripts/click.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
 	$.ajaxSetup({
 		'beforeSend': function (xhr){
-		$.xhr.setRequestHeader("Accept", "text/javascript")}
+		xhr.setRequestHeader("Accept", "text/javascript")}
 	});
 	$("form.remote_for").submit(function(){
 		$('input[type=submit]').attr('disabled', 'disabled');
-		$.post.($(this).attr('action'), $(this).serialize(), null, "script");
+		$.post.$(this).attr('action'), $(this).serialize(), null, "script";
 		return false;
 	});
 }); //document.ready
